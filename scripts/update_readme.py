fdee5196732b6bb947487e535450690149c165cd
Theo <A.theo.0@icloud.com>
2026-02-23 20:17:40 -0500

    Re-align published product row and place App Store badge at title end

--- scripts/update_readme.py
+++ scripts/update_readme.py
@@ -225,16 +225,16 @@ def fmt_app_spotlight_block(app: dict[str, Any]) -> str:
                 f'<a href="{app_url_html}"><img src="{icon_url_html}" width="58" '
                 f'alt="{title_html} icon" align="left" /></a>'
             ),
-            f'&nbsp;<a href="{app_url_html}"><strong>{title_html}</strong></a><br/>',
-            f"&nbsp;<sub>{desc_html}</sub><br/><br/>",
             (
-                f'&nbsp;<img alt="iOS badge" src="{platform_badge}" /> '
+                f'<a href="{app_url_html}"><strong>{title_html}</strong></a>&nbsp;&nbsp;'
+                f'<a href="{app_url_html}"><img alt="Download on the App Store" '
+                f'src="{APP_STORE_BADGE_URL}" height="28" align="absmiddle" /></a><br/>'
+            ),
+            f"<sub>{desc_html}</sub><br/>",
+            (
+                f'<img alt="iOS badge" src="{platform_badge}" /> '
                 f'<img alt="Version badge" src="{version_badge}" /> '
                 f'<img alt="Rating badge" src="{rating_badge}" />'
-            ),
-            (
-                f'<br/>&nbsp;<a href="{app_url_html}"><img alt="Download on the App Store" '
-                f'src="{APP_STORE_BADGE_URL}" height="28" align="absmiddle" /></a>'
             ),
             '<br clear="left"/>',
         ]
